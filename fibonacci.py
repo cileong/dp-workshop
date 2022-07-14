da7c8388f4d3090fdf4fc1ab414928e9ea33827d
Ci Leong Ong <ongcileong2001@gmail.com>
2022-07-14 21:02:07 +0800

    Add DP fibonacci solution

--- fibonacci.py
+++ fibonacci.py
@@ -5,3 +5,16 @@ def recursive_fib(n: int) -> int:
         return 1
     else:
         return recursive_fib(n-1) + recursive_fib(n-2)
+
+
+def dp_fib(n: int) -> int:
+    memo = [0] * max(n, 2)
+    
+    # Base case
+    memo[0] = 0
+    memo[1] = 1
+
+    for i in range(2, n):
+        memo[i] = memo[i-1] + memo[i-2]
+    
+    return memo[-1]
